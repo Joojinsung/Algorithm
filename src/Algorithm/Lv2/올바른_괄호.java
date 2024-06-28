ae107f04707429f4abe458c4b365ff7d3905b510
joojinsung <dev.jinsung1017@gmail.com>
2024-06-28 10:07:15 +0900

    24.06

---
+++ src/Algorithm/Lv2/올바른_괄호.java
@@ -0,0 +1,34 @@
+package Algorithm.Lv2;
+
+import java.util.Objects;
+import java.util.Stack;
+
+public class 올바른_괄호 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        String str = "(())()";
+        System.out.println(solution.solution(str));
+    }
+
+    static class Solution {
+        public boolean solution(String str) {
+            Stack<String> stack = new Stack<>();
+
+            if (str.charAt(str.length() - 1) == '(' || str.charAt(0) == ')') {
+                return false;
+            }
+
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) == '(') {
+                    stack.push(String.valueOf(str.charAt(i)));
+                } else if (!stack.isEmpty()) {
+                    stack.pop();
+
+                }
+            }
+
+            return stack.isEmpty();
+        }
+    }
+}
